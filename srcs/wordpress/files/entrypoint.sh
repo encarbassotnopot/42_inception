dc7f069fbc9a98330fe689dba9cf56ce0699486a
eina <109956262+encarbassotnopot@users.noreply.github.com>
2025-03-25 10:26:50 +0100

    2 usuaris de wp i reinciar segons enunciat

--- srcs/wordpress/files/entrypoint.sh
+++ srcs/wordpress/files/entrypoint.sh
@@ -2,4 +2,10 @@
 
 php84 wp-cli.phar core install --url=$DOMAIN_NAME --title=Inception --admin_user=$WP_USER --admin_password=$WP_PASS --path=wordpress --admin_email="$WP_USER@student.42barcelona.com"
 
+php84 wp-cli.phar user exists editor --path=wordpress
+
+if [ $? -ne 0 ]; then
+	php84 wp-cli.phar user create editor editor@42.fr --path=wordpress;
+fi
+
 php-fpm84 -FO
